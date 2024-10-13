0e1c879a87e3c270e823278ad039672e3d6486f0
Rahul <rahulsm.1503@gmail.com>
2024-10-13 12:22:05 +0530

    minor fixes

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { RotateCw } from "lucide-react";
 const Loading = () => {
   return (
     <div className="flex items-center justify-center">
-      <RotateCw className="animate-spin text-4xl text-gray-500" />;
+      <RotateCw className="animate-spin text-4xl text-gray-500" />
     </div>
   );
 };
